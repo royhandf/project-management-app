17e0d0a6aeb1da9c1568d69ecc809126d2f45d3f
royhandf <royhandaffa14@gmail.com>
2024-12-30 19:18:08 +0700

    feat: add edit button in show project

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -1,19 +1,29 @@
-import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
+import {
+  PROJECT_STATUS_CLASS_MAP,
+  PROJECT_STATUS_TEXT_MAP,
+} from "@/constants.jsx";
 import TasksTable from "../Task/TasksTable";
-
-export default function Show({ auth, project, tasks, queryParams }) {
+export default function Show({ auth, success, project, tasks, queryParams }) {
   return (
     <AuthenticatedLayout
       user={auth.user}
       header={
-        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          {`Project: ${project.name}`}
-        </h2>
+        <div className="flex items-center justify-between">
+          <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {`Project "${project.name}"`}
+          </h2>
+          <Link
+            href={route("project.edit", project.id)}
+            className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+          >
+            Edit
+          </Link>
+        </div>
       }
     >
-      <Head title={`Project: ${project.name}`} />
+      <Head title={`Project "${project.name}"`} />
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -85,6 +95,7 @@ export default function Show({ auth, project, tasks, queryParams }) {
             <div className="p-6 text-gray-900 dark:text-gray-100">
               <TasksTable
                 tasks={tasks}
+                success={success}
                 queryParams={queryParams}
                 hideProjectColumn={true}
               />
